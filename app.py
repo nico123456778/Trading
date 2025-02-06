524847456e3c120b39957b478b429c5b26ed1f6d
nico123456778 <n.schwarz96@icloud.com>
2025-02-06 19:28:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -141,13 +141,12 @@ def get_best_stock():
             return {"error": "Keine Empfehlung verfügbar"}
 
         # Feature-Namen anpassen, damit sie mit dem Modell übereinstimmen
- features = {
-    "MACD": best_stock["MACD"],  # Großbuchstaben verwenden!
-    "RSI": best_stock["RSI"],
-    "SMA200": best_stock["SMA200"],
-    "SMA50": best_stock["SMA50"]
-}
-
+        features = {
+            "MACD": best_stock["MACD"],  # Großbuchstaben verwenden!
+            "RSI": best_stock["RSI"],
+            "SMA200": best_stock["SMA200"],
+            "SMA50": best_stock["SMA50"]
+        }
 
         recommendation = clean_json_data({
             "symbol": best_stock["symbol"],
@@ -163,6 +162,7 @@ def get_best_stock():
     except Exception as e:
         print(f"🔥 FEHLER in /recommendation: {str(e)}")
         return {"error": "Internal Server Error", "details": str(e)}
+
 
 
 @app.get("/")
